310a1819d3de84dc23f7216005b5e430f1bea448
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-01-17 16:14:41 -0800

    Implement getComputerChoice() function

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -24,3 +24,20 @@ function getPlayerChoice() {
       console.log("Invalid input.");
    }
 }
+
+function getComputerChoice() {
+   let computerChoice;
+   let randomNum = Math.floor(Math.random() * 3);
+   switch (randomNum) {
+      case 0: 
+         computerChoice = "rock";
+         break;
+      case 1:
+         computerChoice = "paper";
+         break;
+      case 2:
+         computerChoice = "scissors";
+         break;
+   }
+   return computerChoice;
+}
