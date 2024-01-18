57d5e2cbd716938c093965399699bf4d08ebbe0d
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-01-17 16:10:27 -0800

    Implement getPlayerChoice() function

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -1,6 +1,26 @@
 /*
 Psuedocode:
 Get string input from user.
-Format string input to be case insensitive
-
+Format string input to be case-insensitive
+getComputerChoice function:
+   Generate a random number from 1-3: 1 = rock, 2 = paper, 3 = scissors
+playRound function:
+   Takes 2 arguments: playerChoice and computerChoice and plays a game of rock paper scissosrs
+   Needs to account for ties
+game function:
+Initalize counter var
+Call playRound function 5 times and updates counter var
+Reports a winnwer or loser at the end
 */
+
+function getPlayerChoice() {
+   let playerChoice = prompt("Pick: Rock, Paper, Scissors");
+   playerChoice = playerChoice.toLowerCase();
+
+   if (playerChoice === "rock" || playerChoice ==="paper" || playerChoice === "scissors") {
+      return playerChoice;
+   }
+   else {
+      console.log("Invalid input.");
+   }
+}
